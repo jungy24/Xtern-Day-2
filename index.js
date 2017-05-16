1c082f4a6f71b0e0c468c87c134e5fb632f6a7c7
Your Full Name <jungy24@gmail.com>
2017-05-16 17:50:58 -0400

    Add to beginning of ul

--- index.js
+++ index.js
@@ -7,11 +7,29 @@ const handleSubmit = (ev) => {
 
   const name = form.personName.value
 
-  const ol = document.createElement('ol')
+  const ul = document.createElement('ul')
   const li = document.createElement('li')
-  li.textContent = name
-  list.appendChild(li)
+  li.textContent = name + ' '
+  //li = combine(name, makeButton())
 
+  const button = document.createElement('button')
+  button.innerHTML = 'Highlight ' + name
+  li.appendChild(button)
+
+  list.insertBefore(li, list.childNodes[0])
+
+}
+
+function makeButton(){
+  const button = document.createElement('button')
+  button.innerHTML = 'Highlight Name'
+  return button
+}
+
+function combine(n, b) {
+  const li1 = document.createElement('li')
+  li1.innerHTML = `${n}     ${b}`
+  return li1
 }
 
 rosterForm.addEventListener('submit', handleSubmit)
